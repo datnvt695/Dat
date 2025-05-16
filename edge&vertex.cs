2a98ac2b87c70d2334c919ef8aedfc4fe0bbd14b
datnvt695 <datnvt695@gmail.com>
2025-05-16 20:54:45 +0700

    Update edge&vertex.cs

--- edge&vertex.cs
+++ edge&vertex.cs
@@ -5,32 +5,32 @@ public class Vertex
     public int Id;
     public string Name;
     public int X, Y;
-    public Edge[] Edges = new Edge[100];
-    public int EdgeCount = 0;
+    public Edge[] Edges = new Edge[100]; //Mảng chứa cạnh, tối đa 100 thành phố
+    public int EdgeCount = 0;//Đếm số cạnh tồn tại của thành phố
 
-    public Vertex(int id, string name, int x, int y)
+    public Vertex(int id, string name, int x, int y)// khởi tạo đỉnh (thành phố) mới 
     {
-        Id = id;
-        Name = name;
-        X = x;
+        Id = id;// Thứ tự 0,1,2,3,...
+        Name = name;//Tên thành phố
+        X = x; // Tọa độ X,Y
         Y = y;
     }
 
     public void AddEdge(int to, double weight)
     {
         for (int i = 0; i < EdgeCount; i++)
-            if (Edges[i].To == to) return;
-        Edges[EdgeCount++] = new Edge(to, weight);
+            if (Edges[i].To == to) return; //Kiểm tra xem cạnh đến đỉnh mục tiêu đã tồn tại chưa. Nếu có, không làm gì cả (ngăn chặn trùng lặp).
+        Edges[EdgeCount++] = new Edge(to, weight);//Thêm một cạnh mới từ đỉnh này đến đỉnh khác ('to' là chỉ số của thành phố cần nối đến ), đồng thời lưu trọng số(khoảng cách)
     }
 
     public void RemoveEdgeTo(int to)
     {
-        for (int i = 0; i < EdgeCount; i++)
+        for (int i = 0; i < EdgeCount; i++)//Lặp qua các cạnh hiện tại để tìm cạnh đi tới đỉnh đó
         {
-            if (Edges[i].To == to)
+            if (Edges[i].To == to)//Nếu tìm thấy cạnh thì tiến hành xóa
             {
                 for (int j = i; j < EdgeCount - 1; j++)
-                    Edges[j] = Edges[j + 1];
+                    Edges[j] = Edges[j + 1];//Dịch chuyển phần còn lại của mảng sang trái để lấp đầy khoảng trống của phần tử đã xóa.
                 EdgeCount--;
                 break;
             }
@@ -41,7 +41,7 @@ public class Vertex
     {
         int dx = X - other.X;
         int dy = Y - other.Y;
-        return Math.Sqrt(dx * dx + dy * dy);
+        return Math.Sqrt(dx * dx + dy * dy);//Trả về khoảng cách Euclid đến một thành phố khác bằng cách sử dụng tọa độ của thành phố đó.
     }
 }
 
